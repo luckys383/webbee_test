534ea7243043b6c8f74bbae31b981bc25e69c2a9
Lakhwinder Singh <lakhwinder@logiciel.io>
2022-06-12 21:20:49 +0530

    Task 2: Future events task is completed

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -8,6 +8,8 @@ use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\Date;
+use App\Models\Workshop;
+use Carbon\Carbon;
 
 class EventsController extends BaseController
 {
@@ -181,6 +183,15 @@ class EventsController extends BaseController
      */
 
     public function getFutureEventsWithWorkshops() {
-        throw new \Exception('implement in coding task 2');
+        $workshopTableaName = (new Workshop)->getTable();
+        $eventTableaName = (new Event)->getTable();
+        $items = Event::join("$workshopTableaName as w", 'w.event_id', '=', "$eventTableaName.id")
+                    ->with(['workshops'])
+                    ->where("w.start", '>', Carbon::now())
+                    ->select("$eventTableaName.*")
+                    ->groupBy("$eventTableaName.id")
+                    ->get();
+        return $items;
+        // throw new \Exception('implement in coding task 2');
     }
 }
